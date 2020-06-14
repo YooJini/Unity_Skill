c4f2546bf4ee62fc0bc41aedc1e5b722e1e05ca8
YooJini <wlsdl6685@gmail.com>
2020-06-15 00:06:02 +0900

    3. 폭발 파편
     - 유니티버전: Unity 2019.3.13f1(64-bit)
     -  비주얼스튜디오: 2019 community

---
+++ New Unity Project/Assets/_JINI/Scripts/Cube2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube2 : MonoBehaviour
+{
+    [SerializeField] GameObject goPrefab = null;
+    [SerializeField] float force = 0f;
+    [SerializeField] Vector3 offset = Vector3.zero;
+
+    public void Explosion()
+    {
+        GameObject clone = Instantiate(goPrefab, transform.position, Quaternion.identity);
+        Rigidbody[] rigids = clone.GetComponentsInChildren<Rigidbody>();
+        for (int i = 0; i < rigids.Length; i++)
+        {
+            rigids[i].AddExplosionForce(force, transform.position + offset, 10f);
+        }
+        gameObject.SetActive(false);
+    }
+}
---
+++ New Unity Project/Assets/_JINI/Scripts/MouseController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngineInternal;
+
+public class MouseController : MonoBehaviour
+{
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit, 100f))
+            {
+                hit.transform.GetComponent<Cube2>().Explosion();
+            }
+        }
+    }
+}
