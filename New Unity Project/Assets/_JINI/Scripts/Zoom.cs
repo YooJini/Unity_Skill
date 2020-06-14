32b7a844bce4962920befccbe6c78547abfddbe3
YooJini <wlsdl6685@gmail.com>
2020-06-15 00:31:19 +0900

    8. 마우스 휠 카메라 줌
     - 유니티버전: Unity 2019.3.13f1(64-bit)
     -  비주얼스튜디오: 2019 community

---
+++ New Unity Project/Assets/_JINI/Scripts/Zoom.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoom : MonoBehaviour
+{
+    [SerializeField] float speed = 0f;
+    [SerializeField] float max = 0f;
+    [SerializeField] float min = 0f;
+
+    void CameraZoom()
+    {
+        float zoomDirection = Input.GetAxis("Mouse ScrollWheel");
+
+        if (transform.position.y <= max && zoomDirection > 0)
+            return;
+        if (transform.position.y >= min && zoomDirection < 0)
+            return;
+
+        transform.position += transform.forward * zoomDirection * speed;
+    }
+    void camMove()
+    {
+        if(Input.GetMouseButton(2))
+        {
+            float posX = Input.GetAxis("Mouse X");
+            float posZ = Input.GetAxis("Mouse Y");
+            transform.position += new Vector3(posX, 0, posZ);
+
+        }    
+    }
+    private void Update()
+    {
+        CameraZoom();
+        camMove();
+
+
+    }
+}
